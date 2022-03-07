fcb120ad9888af83e3427f60fc51fa598a4882c1
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-07 19:19:04 +0300

    Add files via upload

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Struct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Dikdörtgen dikdörtgen = new Dikdörtgen();
+          dikdörtgen.kisaKenar = 3;
+          dikdörtgen.uzunKenar = 5;
+
+          Console.WriteLine("Class Alan Hesabı :" + dikdörtgen.AlanHesapla());
+
+          Dikdörtgen_Struct dikdörtgen_Struct = new Dikdörtgen_Struct();
+          dikdörtgen_Struct.kisaKenar = 5;
+          dikdörtgen_Struct.uzunKenar = 6;
+          Console.WriteLine("Class Alan Hesabı :" + dikdörtgen_Struct.AlanHesapla());
+
+
+        }
+    }
+
+    class Dikdörtgen
+    {
+        public int kisaKenar;
+
+        public int uzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+    }
+
+    struct Dikdörtgen_Struct
+    {
+        public int kisaKenar;
+
+        public int uzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+
+    }
+}
