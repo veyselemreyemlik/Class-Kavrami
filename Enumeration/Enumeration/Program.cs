f3c30a38a5e4d512a65af826e0d0ce427fe0fdf2
Veysel Emre Yemlik <emreyemlik1905@gmail.com>
2022-03-07 20:49:42 +0300

    Add files via upload

---
+++ Enumeration/Enumeration/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Enumeration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)(Gunler.Cumartesi));
+
+            int sıcaklık = 25;
+
+            if(sıcaklık <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Dışarıya Çıkmak İçin Havanın Biraz Daha Isınmasını Bekleyelim.");
+            }
+            else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+                Console.WriteLine("Dışarıya Çıkmak İçin Çok Sıcak Bir Gün");
+            else if(sıcaklık >= (int)HavaDurumu.Normal && sıcaklık< (int)HavaDurumu.ÇokSıcak)
+            {
+                Console.WriteLine("Hadi Dışarıya Çıkalım.");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+        
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5 ,
+        Normal = 20,
+        Sıcak = 25 ,
+        ÇokSıcak = 30 
+    }
+}
